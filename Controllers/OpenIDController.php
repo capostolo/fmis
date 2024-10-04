128287d1286cc0d31f8db39da2b5bf06565845c1
capostolo <costas_a@yahoo.com>
2024-10-04 16:21:32 +0200

    OpenID3

--- Controllers/OpenIDController.php
+++ Controllers/OpenIDController.php
@@ -13,23 +13,29 @@ class OpenIDController extends BaseController
   public function index()
   {
 	$issuer = 'https://accounts.opekepe.gr/realms/eco-schemes';
+	$provider = 'https://accounts.opekepe.gr/realms/eco-schemes';
 	$cid = 'schemis';
-	$secret = '04C6n2ZtfuoL82cFG9Q15IF55o1SV98x';
-	$oidc = new \Jumbojett\OpenIDConnectClient($issuer, $cid, $secret);
+	$secret = '04C6n2ZtfuoL82cFG9Ql5IF55olSV98x';
+	$oidc = new \Jumbojett\OpenIDConnectClient($cid, $secret, $issuer, $provider);
 
 	$oidc->authenticate();
-	$oidc->requestUserInfo();
+	$token = $oidc->access_token;
+	$afm = $oidc->tin;
+	$options = [
+		'baseURI' => 'https://eae.opekepe.gr/api/v1/',
+		'headers' => [
+			'Authorization' => 'Bearer '. $token,
+		],
+		'query' => [
+			'tin' => $afm, 
+			'year' => '2024', 
+			'initial' => 'false'
+		],		
+	];
+	$client = service('curlrequest', $options);
+	$response = $client->get('applications');
+	var_dump($response);
 
-	$attributes = array();
-	foreach($oidc as $key=> $value) {
-		if(is_array($value)){
-				$v = implode(', ', $value);
-		}else{
-				$v = $value;
-		}
-		$attributes[$key] = $v;
-	}
-	session()->set('openid_attr', $attributes);
 
   }
 }
