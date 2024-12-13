9c21a1d94b9c7de98803a0121f8df45e13a47530
Costas Apostolopoulos <costas_a@yahoo.com>
2024-12-13 12:14:53 +0200

    Allow farmer location in allowed fields

--- Models/FarmerModel.php
+++ Models/FarmerModel.php
@@ -13,7 +13,7 @@ class FarmerModel extends Model
   protected $returnType     = 'Fmis\Entities\FarmerEntity';
   protected $useSoftDeletes = false;
 
-  protected $allowedFields = ['farmer_afm', 'farmer_firstname', 'farmer_lastname', 'farmer_fathername', 'farmer_mobile', 'farmer_email', 'farmer_dtebirth', 'advisor_id', 'user_id'];
+  protected $allowedFields = ['farmer_afm', 'farmer_firstname', 'farmer_lastname', 'farmer_fathername', 'farmer_mobile', 'farmer_email', 'farmer_dtebirth', 'farmer_location', 'advisor_id', 'user_id'];
 
   protected $useTimestamps = true;
 
